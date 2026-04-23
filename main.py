2a0b2d829a075ba57c56c5b57d68428461286448
emporomenkou <noobprohacker@tutamail.com>
2026-04-23 22:17:28 +0300

    dafgae

---
+++ main.py
@@ -0,0 +1,5 @@
+from polymorf_lz import Dataframe
+
+df = Dataframe('var8.csv')
+-df
+df.grouping()
--- polymorf_lz.py
+++ polymorf_lz.py
@@ -1,16 +1,14 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
 
 
-class Datasets:
+class Dataframe:
     def __init__(self, filename: str):
         self.__df = pd.read_csv(filename, sep = ',')
-    def getdata(self):
-        a = self.__df.groupby('Результат операции')
-        #return a
-        print(a.get_group('неправильный ввод пин-кода'))
-        print(self.__df.duplicated().sum())
-        print(self.__df[self.__df.duplicated()])
-dataset = Datasets('var8.csv')
-dataset.getdata()
+    def grouping(self):
+        a = self.__df.groupby('Результат операции') #группируем
+        for type, group in a: #type - параметр, group - датафрейм имеющий этот параметр
+            group.to_csv(f"результат-операции_{type}.csv", index = False) #заносим результат группировки в разные файлы
+    def __neg__(self):
+        print(f"Количество повторяющихся строк: {self.__df.duplicated().sum()}") #выводим кол-во дубликатов
+        self.__df.drop_duplicates(inplace = True)
+
